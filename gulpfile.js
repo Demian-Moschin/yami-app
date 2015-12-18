03d6894ff0233169e57eb4133f458f34d64dbda9
Demian Moschin <Demian.Moschin@wnco.com>
2015-12-18 17:47:31 -0300

    new files

---
+++ gulpfile.js
@@ -0,0 +1,73 @@
+var browserify = require('browserify');
+var browserSync = require('browser-sync');
+var concat = require('gulp-concat');
+var gulp = require('gulp');
+var handlebars = require('gulp-compile-handlebars');
+var reactify = require('reactify');
+var run = require('run-sequence');
+var sass = require('gulp-sass');
+var source = require('vinyl-source-stream');
+
+gulp.task('sass', function () {
+    var bundle = gulp.src('./scss/styles.scss').pipe(sass()).on('error', function (error) {
+        console.log(error);
+        this.emit('end');
+    });
+
+    return bundle.pipe(concat('style.scss')).pipe(gulp.dest('build/css/'));
+});
+
+gulp.task('sources', function () {
+
+    return gulp.src(['./images/**/*'])
+        .pipe(gulp.dest('build/images'));
+});
+
+gulp.task('js', function () {
+    var bundle = browserify('./index.js').transform(reactify).bundle().on('error', function (error) {
+        console.log('no se pudo load el js !!!' + error);
+        this.emit('end');
+    });
+
+    return bundle.pipe(source('./js/app.js')).pipe(gulp.dest('build/js'));
+});
+
+gulp.task('html', function () {
+    var bundle = gulp.src('./html/index.html').pipe(handlebars()).on('error', function (error) {
+        console.log(error);
+        this.emit('end');
+    });
+
+    return bundle.pipe(gulp.dest('build/'));
+});
+
+gulp.task('browser-sync', function () {
+    browserSync.init({
+        open: false,
+        server: {
+            baseDir: 'build/'
+        },
+        port: '8080'
+    });
+
+    gulp.watch([
+        'stores/**/*.js',
+        'index.js',
+        'components/**/*.js',
+        'libs/**/*.js',
+        'views/**/*.js'
+    ], ['js', browserSync.reload]);
+    gulp.watch([
+        'html/**/*.html'
+    ], ['html', browserSync.reload]);
+    gulp.watch([
+        'components/**/*.scss',
+        'sass/**/*.scss',
+        'views/**/*.scss'
+    ], ['sass', browserSync.reload]);
+});
+
+gulp.task('default', function (cb) {
+    run = require('run-sequence').use(gulp);
+    run('html', 'js', 'sass', 'sources', 'browser-sync', cb);
+});
